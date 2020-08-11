16dbb39456912f7b93e655ca6720f401db33db2e
Jun Lee <jy45lee@gmail.com>
2020-08-10 21:12:20 -0700

    fixed POST test

--- backend/tests/coffee_api.test.js
+++ backend/tests/coffee_api.test.js
@@ -63,7 +63,8 @@ describe("POST", () => {
     expect(jsonStrings).toContain(JSON.stringify(res.body))
   })
 
-  test("with new beans creates doc in DB", async () => {
+  test.only("with new beans creates doc in DB", async () => {
+    await Bean.deleteMany({})
     const coffeeWithNewBean = {
       roastDate: "2020-09-01",
       origin: "Guatamala",
